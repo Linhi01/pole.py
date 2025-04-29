b88f2a093112fa7c841b75d3085ad5a5a4ce1414
Linhi01 <dominik.linhart@infis.cz>
2025-04-29 08:38:18 +0200

    Add files via upload

---
+++ pole.py
@@ -0,0 +1,16 @@
+
+#skore zapasu
+pole = [1,5,10,12,20,30,40,50,55,60]
+
+print(sum(pole))
+
+print(sum(pole)/10)
+
+print(max(pole))
+print(min(pole))
+
+hraci = len(pole)
+polovinahracu = hraci/2
+
+if polovinahracu>50:
+    print("Výborný výkon!")
