8c142d14c0c41d0cfd0a78cbf2a07ff16c5f4364
Linhi01 <dominik.linhart@infis.cz>
2025-05-13 11:26:57 +0200

    Add files via upload

---
+++ pole na test.py
@@ -0,0 +1,68 @@
+#ukol 1 
+#jablko
+#pomeranč
+#hruška
+ 
+# ukol 2 - vypsání jídla pomocí printu
+jidla = ["kebab", "kureci", "klobasy s jalapenos"]
+print(jidla[0])
+print(jidla[1])
+print(jidla[-1])
+ 
+# ukol 3 - soucet známek a délka textu
+AAP = [1, 3, 2, 1, 3, 1, 1, 1]
+soucet = sum(AAP)
+print(soucet)
+ 
+pocetZnamek = len(AAP)
+print(pocetZnamek)
+ 
+# ukol 4 - vypsání 2 a 4 pole
+zvirata = ["pes", "kocour v botach", "slon", "mys"]
+print(zvirata[1])
+print(zvirata[-1])
+ 
+# ukol 5 - změna seznamu(přidání čísel)
+seznam = [1, 2, 3]
+print(seznam)
+seznam.append(4)
+seznam.append(5)
+seznam.append(6)
+print(seznam)
+ 
+# ukol 6 - vypsání filmů
+filmy = ["Rychle a Zběsile", "Avengers", "Harry potter"]
+print(filmy[0])
+print(filmy[1])
+print(filmy[2])
+ 
+# ukol 7 - nahrazení hodnoty
+cisla = [1, 2, 3]
+print(cisla)
+cisla[0] = 999
+print(cisla)
+ 
+# ukol 8 - sečtení libovolných polí
+seznamCisel = [1, 2, 3, 4, 5]
+soucet = sum(seznamCisel)
+print(soucet)
+
+#ukol 9 - vypsání velikosti pole
+jakekolivPole = ["pes","kočka","myš",1,2,3]
+delkaPole = len(jakekolivPole)
+print(delkaPole)
+
+#ukol 10 - průměr čísel(známek)
+znamky = [1,4,3,2,1,2,2,4]
+hodnota = (sum(znamky))
+delka = (len(znamky))
+print(hodnota/delka)
+
+#ukol 11 - zjištění daného výrazu(slova "banán")
+ovocee = ["meloun","kiwi","angrešt"]
+if "banán" in ovocee:
+    print("banán tam je oujeeee")
+if not
+    print("banán tam není")
+
+#ukol 12
